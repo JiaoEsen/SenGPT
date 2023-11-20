9c1a21f1017f32c26e84769f4db3dbbeb27ebfb7
jiaoyisen <18210614234@163.com>
2023-11-20 15:09:58 +0800

    add router、pinia、sass

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router/index'
+import pinia from './store'
 import 'amfe-flexible'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
