55b97cbd6a979ba030219d6e467be218c434544b
jiaoyisen <18210614234@163.com>
2023-11-20 14:47:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="box"></div>
+</template>
+
+<style scoped>
+.box {
+  width: 100%;
+  height: 70px;
+  background-color: red;
+}
+</style>
