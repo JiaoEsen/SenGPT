56bb70572cc51241aede7f7820452734a8bf34ae
jiaoyisen <18210614234@163.com>
2023-11-20 14:57:44 +0800

    add vant

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
 </script>
 
 <template>
-  <div class="box"></div>
+  <div class="box">
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="success">成功按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <style scoped>
